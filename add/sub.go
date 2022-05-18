867ad3bc14dbdfcaa4606c9035c25ecc3e40f8fe
hailaz <739476267@qq.com>
2022-05-18 11:08:13 +0800

    update

--- add/sub.go
+++ add/sub.go
@@ -7,3 +7,12 @@ func Add(x, y int) int {
 func Subtract(x, y int) int {
 	return x - y
 }
+
+// Hi description
+//
+// createTime: 2022-05-18 11:07:43
+//
+// author: hailaz
+func Hi(who string) string {
+	return "Hi " + who
+}
