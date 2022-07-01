5593451db08d73e77f8e1142078fdff41e49430b
hailaz <739476267@qq.com>
2022-07-01 17:23:14 +0800

    111

--- gotest_test.go
+++ gotest_test.go
@@ -1,9 +1,13 @@
 package gotest
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestAdd(t *testing.T) {
 	if got, want := Add(1, 2), 3; got != want {
 		t.Errorf("add method produced wrong result. expected: %d, got: %d", want, got)
 	}
+	time.Sleep(time.Second * 60)
 }
